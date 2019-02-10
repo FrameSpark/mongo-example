efed856b288132ab78ffb3eb9869660e924a4f72
framespark <432032@mail.ru>
2019-02-10 22:32:16 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 import java.net.UnknownHostException;
@@ -8,5 +11,14 @@ public class Main {
         //MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
         //or
         MongoClient mongoClient = new MongoClient(); //Создаем соединение с базой. Такой способ для локалки автомитически. Сверху для подключения к хосту и порту.
-    }
+
+        DB database = mongoClient.getDB("MyData"); //Берем базу по названию
+        DBCollection table = database.getCollection("Users"); //Берем таблицу по названию
+}
+
+public void addToDB(User user, DBCollection table) {
+    BasicDBObject object = new BasicDBObject();
+    object.put("id", user.getId());
+    object.put("name", user.getName());
+    table.insert(object);
 }
